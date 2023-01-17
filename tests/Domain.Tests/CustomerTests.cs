29bf77fbaf4e21ccf2d53f9b671a968be122958d
AnoEma <anoemangi@gmail.com>
2023-01-17 19:04:33 -0300

    Add testes

---
+++ tests/Domain.Tests/CustomerTests.cs
@@ -0,0 +1,14 @@
+using Domain.Entities;
+namespace Domain.Tests;
+
+public class CustomerTests
+{
+    [Fact]
+    public void Create_Customer_Give_Model()
+    {
+        var entitie = new Customer(){
+
+        };
+
+    }
+}
---
+++ tests/Domain.Tests/PetTests.cs
@@ -0,0 +1,13 @@
+namespace Domain.Tests;
+
+public class PetTests
+{
+    [Fact]
+    public void Create_Pet_Give_Model()
+    {
+        var entitie = new Pet(){
+            
+        };
+
+    }
+}
